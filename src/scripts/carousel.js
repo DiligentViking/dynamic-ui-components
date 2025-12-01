9b7671e9dda8e7d82a5e4da1e49a53129bbae8bf
DiligentViking <acore-n@proton.me>
2025-12-01 16:20:55 -0600

    Add base carousel HTML/JS

---
+++ src/scripts/carousel.js
@@ -0,0 +1,2 @@
+export function addCarouselBehavior(carouselContainer, carouselContent) {
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,7 @@
 import { addDropdownBehavior } from './dropdown.js';
+import { addCarouselBehavior } from './carousel.js';
 
 addDropdownBehavior(document.querySelector('.dropdown-elem'), document.querySelector('.dropdown-list'));
+// TODO: Test with multiples
+
+addCarouselBehavior(document.querySelector('.carousel-container'), document.querySelector('.carousel-content'));
