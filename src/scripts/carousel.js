bbab1bee7310d14169251a4ec728f5447975954a
DiligentViking <acore-n@proton.me>
2025-12-01 19:43:47 -0600

    Add main carousel CSS and movement JS

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,2 +1,20 @@
-export function addCarouselBehavior(carouselContainer, carouselContent) {
+export function addCarouselBehavior(carouselContainer, carouselContent, options={gap}) {
+  carouselContent.style.gap = `${options.gap}px`;
+  carouselContent.style.transition = 'translate 1s ease';
+
+  let slideIndex = 0;
+  let distanceMoved = 0;
+
+  const slides = carouselContent.children;
+
+  function next() {
+    const slideRect = slides[slideIndex].getBoundingClientRect();
+    const spaceToMove = slideRect.width + options.gap;
+    distanceMoved += spaceToMove;
+    carouselContent.style.translate = `-${distanceMoved}px`;
+    slideIndex++;
+  }
+
+  next();  // Dev
+  setInterval(next, 5000);
 }
